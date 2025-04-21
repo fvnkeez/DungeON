d88fe6216275ca6a0a15d9f742918ecaff65c8a8
Daniel Fuente <danifuente1@gmail.com>
2025-04-21 14:10:57 +0200

    Pass de mysql

--- src/bbdd/ConexionDB.java
+++ src/bbdd/ConexionDB.java
@@ -8,7 +8,7 @@ public class ConexionDB {
 
     private static final String URL = "jdbc:mysql://localhost:3306/Dungeon";
     private static final String USER = "root";
-    private static final String PASSWORD = "Password1234";
+    private static final String PASSWORD = "1234";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
